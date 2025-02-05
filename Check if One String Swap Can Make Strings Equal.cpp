cc9dffe5eb5558609c5e289d80cfeda9f1386a03
NilayBadjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-02-05 17:02:36 +0530

    Update Check if One String Swap Can Make Strings Equal.cpp

--- Check if One String Swap Can Make Strings Equal.cpp
+++ Check if One String Swap Can Make Strings Equal.cpp
@@ -16,3 +16,27 @@ public:
         return s1 == s2;
     }
 };
+
+// Optimal Approach
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.size(), count = 0;
+        int sw1 = 0, sw2 = 0;
+        for(int i = 0; i < n; i++){
+            if(s1[i] != s2[i]){
+                if(count == 0){
+                    sw1 = i;
+                }else{
+                    sw2 = i;
+                }
+                count++;
+            }
+            if(count >= 3){
+                return false;
+            }
+        }
+        swap(s2[sw1], s2[sw2]);
+        return s1 == s2;
+    }
+};
