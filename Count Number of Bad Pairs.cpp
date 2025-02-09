7f9c913a6cff97d02c5135e66bc7f0f23d48e6b4
NilayBadjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-02-09 14:34:07 +0530

    Update Count Number of Bad Pairs.cpp

--- Count Number of Bad Pairs.cpp
+++ Count Number of Bad Pairs.cpp
@@ -14,3 +14,21 @@ public:
         return count;
     }
 };
+
+// Approach 2:- Space Optimized
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        long long n = nums.size();
+        unordered_map<long long, long long> mp;
+        for(long long i = 0; i < n; i++){
+            mp[nums[i] - i]++;
+        }
+        long long count = 0;
+        for(long long i = 0; i < n; i++){
+            count += n - (i + 1) - (mp[nums[i] - i] - 1);
+            mp[nums[i] - i]--;
+        }
+        return count;
+    }
+};
